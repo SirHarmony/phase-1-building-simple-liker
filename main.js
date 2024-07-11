dcf508c905152d49b219bbf0d68a5c4490109c83
Harmony <harmonymukolwe@gmail.com>
2024-07-12 00:51:26 +0300

    Completes lab

--- main.js
+++ main.js
@@ -1,20 +1,27 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = "♡";
+const FULL_HEART = "♥";
 
 // Your JavaScript code goes here!
-
-
-
+const likeGlyph = document.querySelector(".like-glyph");
+likeGlyph.addEventListener(
+  "click",
+  mimicServerCall()
+    .then((data) => data)
+    .catch((error) => {
+      const errorModal = document.querySelector("div#modal h2");
+      errorModal.classList.remove("hidden");
+    })
+);
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      let isRandomFailure = Math.random() < 0.2;
       if (isRandomFailure) {
         reject("Random server error. Try again.");
       } else {
